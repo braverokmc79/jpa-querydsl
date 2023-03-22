59cb5d3ecae54613841dcea239ae81cf12672f09
braverokmc79 <braverokmc79@gmail.com>
2023-03-22 21:09:53 +0900

    5.라이브러리 살펴보기

--- src/test/java/study/querydsl/ApplicationTests.java
+++ src/test/java/study/querydsl/ApplicationTests.java
@@ -1,13 +1,60 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+import javax.persistence.EntityManager;
 
 @SpringBootTest
+@Transactional
+@Rollback(value = false)
 class ApplicationTests {
+
+	@Autowired
+	EntityManager em;
+
+
+	@Autowired
+	JPAQueryFactory jpaQueryFactory;
+
 
 	@Test
 	void contextLoads() {
+		Hello hello=new Hello();
+		em.persist(hello);
+
+
+		JPAQueryFactory query=new JPAQueryFactory(em);
+		//QHello qHello=new QHello("h");
+		QHello qHello=QHello.hello;  //Querydsl Q타입 동작 확인
+		Hello result =query.selectFrom(qHello)
+				.fetchOne();
+
+		Assertions.assertThat(result).isEqualTo(hello);
+		//lombok 동작 확인(hello.getId());
+		Assertions.assertThat(result.getId()).isEqualTo(hello.getId());
+	}
+
+
+	@Test
+	void contextLoads2() {
+		Hello hello=new Hello();
+		em.persist(hello);
+
+
+		QHello qHello=QHello.hello;
+		Hello result =jpaQueryFactory.selectFrom(qHello).fetchOne();
+
+		Assertions.assertThat(result).isEqualTo(hello);
+		//lombok 동작 확인(hello.getId());
+		Assertions.assertThat(result.getId()).isEqualTo(hello.getId());
 	}
 
 }

44a1bcaa1f7861a444f5997773d7e4557307c5ab
braverokmc79 <braverokmc79@gmail.com>
2023-03-22 19:13:03 +0900

    3.프로젝트 생성

---
+++ src/main/java/study/querydsl/Application.java
@@ -0,0 +1,28 @@
+package study.querydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+import java.util.UUID;
+
+//@EnableJpaAuditing(modifyOnCreate = false) //생성시에는  modifyOnCreate null 값처리
+@EnableJpaAuditing(modifyOnCreate = false)
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+
+	@Bean
+	public AuditorAware<String>  auditorProvider(){
+		return () -> Optional.of(UUID.randomUUID().toString());
+	}
+
+
+}

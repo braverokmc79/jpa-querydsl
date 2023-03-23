658191fd8baaf634ffa8be445af3c1dd27bdbb42
braverokmc79 <braverokmc79@gmail.com>
2023-03-23 21:11:03 +0900

    9.시작 - JPQL vs Querydsl

--- src/test/java/study/querydsl/ApplicationTests.java
+++ src/test/java/study/querydsl/ApplicationTests.java
@@ -5,6 +5,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Hello;
@@ -14,28 +15,26 @@ import javax.persistence.EntityManager;
 
 @SpringBootTest
 @Transactional
-@Rollback(value = false)
+//@Rollback(value = false)
+@Commit
 class ApplicationTests {
 
 	@Autowired
 	EntityManager em;
 
-
 	@Autowired
 	JPAQueryFactory jpaQueryFactory;
-
 
 	@Test
 	void contextLoads() {
 		Hello hello=new Hello();
 		em.persist(hello);
 
-
 		JPAQueryFactory query=new JPAQueryFactory(em);
 		//QHello qHello=new QHello("h");
 		QHello qHello=QHello.hello;  //Querydsl Q타입 동작 확인
-		Hello result =query.selectFrom(qHello)
-				.fetchOne();
+		Hello result =query.selectFrom(qHello).fetchOne();
+
 
 		Assertions.assertThat(result).isEqualTo(hello);
 		//lombok 동작 확인(hello.getId());

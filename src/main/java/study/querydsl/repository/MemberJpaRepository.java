02c38e9539069caee3ff448df3016a43835e788a
braverokmc79 <braverokmc79@gmail.com>
2023-03-27 21:13:50 +0900

    32.동적 쿼리와 성능 최적화 조회 - Where절 파라미터 사용

--- src/main/java/study/querydsl/repository/MemberJpaRepository.java
+++ src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -2,6 +2,8 @@ package study.querydsl.repository;
 
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.annotations.QueryProjection;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -99,6 +101,42 @@ public class MemberJpaRepository{
                 .fetch();
     }
 
+
+    public List<MemberTeamDto> search(MemberSearchCondition condition){
+        return queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")
+                ))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoeEq(condition.getAgeGoe()),
+                        ageLoeEq(condition.getAgeLoe())
+                )
+                .fetch();
+    }
+
+    private BooleanExpression usernameEq(String username) {
+        return hasText(username) ?  member.username.eq(username) :null;
+    }
+
+    private BooleanExpression teamNameEq(String teamName) {
+        return hasText(teamName) ? team.name.eq(teamName) :null;
+    }
+
+    private BooleanExpression ageGoeEq(Integer ageGoe) {
+        return ageGoe!=null? member.age.goe(ageGoe) : null;
+    }
+
+    private BooleanExpression ageLoeEq(Integer ageLoe) {
+        return ageLoe!=null? member.age.loe(ageLoe) : null;
+    }
 
 
 
